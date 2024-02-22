42c42e9ff18fd25ff7dc8b194e52b0785d20eda4
Fred Oliveira <fred@helloform.com>
2024-02-22 17:59:08 +0000

    Cleanup, tool versions

--- src/api/index.py
+++ src/api/index.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/api/python")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return jsonify({"message": "Hello, World!"})
