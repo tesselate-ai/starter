007b5ac16dcc7eab608007c51b84f71b20e3ce52
Fred Oliveira <fred@helloform.com>
2024-02-22 17:12:42 +0000

    API URL rewrites, gitignore rules

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: process.env.NODE_ENV === "development" ? "http://127.0.0.1:5328/api/:path*" : "/api/",
+      },
+    ];
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
